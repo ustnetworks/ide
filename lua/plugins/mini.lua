c9ddfb561040604a962e0d5c95cc39f916ccd0a2
ustnetworks <58238716+ustnetworks@users.noreply.github.com>
2026-02-01 08:23:03 -0800

    Update mini.lua

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,10 +3,8 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("mini.ai").setup()
-		require("mini.surround").setup()
 		require("mini.move").setup()
 		require("mini.indentscope").setup()
 		require("mini.files").setup()
-		require("mini.animate").setup({ scroll = { enable = false }, cursor = { enable = false } })
 	end,
 }
