a4c2f8c66ff2ffd7251714fdc3fb19724366fc32
ustnetworks <58238716+ustnetworks@users.noreply.github.com>
2026-02-01 08:29:54 -0800

    Update mini.lua

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,9 +2,7 @@ return {
 	"echasnovski/mini.nvim",
 	event = "VeryLazy",
 	config = function()
-		require("mini.ai").setup()
 		require("mini.move").setup()
-		require("mini.indentscope").setup()
 		require("mini.files").setup()
 	end,
 }
